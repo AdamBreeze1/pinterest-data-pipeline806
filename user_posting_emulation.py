0b1303f97967a49a4d8cb4b5dfde79716f33116d
Adam <adambreeze99@gmail.com>
2024-01-09 16:25:26 +0000

    fix security risk

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -1,30 +1,12 @@
 import requests
 from time import sleep
 import random
-from multiprocessing import Process
-import boto3
 import json
-import sqlalchemy
 from sqlalchemy import text
+from AWSDBConnector_creds import AWSDBConnector
 
 
 random.seed(100)
-
-
-class AWSDBConnector:
-
-    def __init__(self):
-
-        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
-        self.USER = 'project_user'
-        self.PASSWORD = ':t%;yCY3Yjg'
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
-        
-    def create_db_connector(self):
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
-        return engine
-
 
 new_connector = AWSDBConnector()
 
